2487b7d3c460e3409dd0365381fe49bbcfe49fbe
Vedant Bairagi <bairagivg19.comp@coep.ac.in>
2022-08-28 23:22:52 +0530

    changes;

--- Prediction App.py
+++ Prediction App.py
@@ -17,15 +17,15 @@ clarity_d = {col: index+1 for index, col in enumerate(['I1', 'SI2', 'SI1', 'VS2'
 st.sidebar.header('Specify Input Parameters')
 
 def user_input():
-    carat = st.sidebar.slider('Carat Value', df['carat'].min(), df['carat'].max(), df['carat'].mean())
+    carat = st.sidebar.slider('Carat Value', min_value=df['carat'].min(), max_value=df['carat'].max(), step=df['carat'].mean(), value=0.23)
     cut = st.sidebar.selectbox('Cut Type', ['Fair','Good','Very Good','Premium','Ideal'])
     color = st.sidebar.selectbox('Color', ['J', 'I', 'H', 'G', 'F', 'E', 'D'])
     clarity = st.sidebar.selectbox('Clarity', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
-    depth = st.sidebar.slider('Depth Value', df['depth'].min(), df.depth.max(), df.depth.mean())
-    table = st.sidebar.slider('Table Value', df.table.min(), df.table.max(), df.table.mean())
-    x = st.sidebar.slider('X (Length mm)', df['x'].min(), df['x'].max(), df['x'].mean())
-    y = st.sidebar.slider('Y (Width mm)', df['y'].min(), df['y'].max(), df['y'].mean())
-    z = st.sidebar.slider('Z (Depth mm)', df['z'].min(), df['z'].max(), df['z'].mean())
+    depth = st.sidebar.slider('Depth Value', min_value=df['depth'].min(), max_value=df.depth.max(), step=df.depth.mean(), value=61.0)
+    table = st.sidebar.slider('Table Value', min_value=df.table.min(), max_value=df.table.max(), step=df.table.mean(), value=55.1)
+    x = st.sidebar.slider('X (Length mm)', min_value=df['x'].min(), max_value=df['x'].max(), step=df['x'].mean(), value=3.85)
+    y = st.sidebar.slider('Y (Width mm)', min_value=df['y'].min(), max_value=df['y'].max(), step=df['y'].mean(), value=4.23)
+    z = st.sidebar.slider('Z (Depth mm)', min_value=df['z'].min(), max_value=df['z'].max(), step=df['z'].mean(), value=2.4)
 
     sample = pd.DataFrame({'carat':carat, 'cut':cut, 'color':color,
     'clarity':clarity, 'depth':depth, 'table':table, 'x':x, 'y':y, 'z':z}, index=[0])
